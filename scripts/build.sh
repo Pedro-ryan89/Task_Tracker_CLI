aba5b834b1c662322c02e4764a2d2cf756aa3d53
Pedro Ryan <120953398+Pedro-ryan89@users.noreply.github.com>
2026-04-03 20:19:33 -0300

    Delete scripts directory

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/.."
-
-javac -d out/production/Task_Tracker src/*.java
--- scripts/task.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/.."
-
-java -cp "out/production/Task_Tracker" Main "$@"
